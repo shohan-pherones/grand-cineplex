2507a648ca424a89570b879301fd97bd8853c06f
shohan-pherones <shohan.sub.56@gmail.com>
2023-12-13 21:03:42 +0600

    movie deleted

--- src/pages/ManageMoviesPage.jsx
+++ src/pages/ManageMoviesPage.jsx
@@ -1,19 +1,45 @@
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import AdminProtection from "../components/AdminProtection";
 import Error from "../components/Error";
 import Loading from "../components/Loading";
 import { useFetch } from "../hooks/useFetch";
+import { axiosDelete } from "../lib/axiosDelete";
 
 const ManageMoviesPage = () => {
+  const [allMovies, setAllMovies] = useState([]);
+  const [shouldDeleteConfirmationApear, setShouldDeleteConfirmationApear] =
+    useState(null);
   const auth = useSelector((state) => state.auth.userAndToken);
   const navigate = useNavigate();
 
   const { data: movies, isLoading, error } = useFetch("/api/movies");
 
+  useEffect(() => {
+    if (movies.length > 0) {
+      setAllMovies(movies);
+    }
+  }, [movies]);
+
   if (auth?.user?.role !== "admin") {
     return <AdminProtection />;
   }
+
+  const handleDelete = async () => {
+    const data = await axiosDelete(
+      `/api/movies/${shouldDeleteConfirmationApear}`,
+      auth?.token
+    );
+
+    if (data) {
+      toast.success("Movie deleted successfully");
+      setAllMovies(
+        allMovies.filter((mv) => mv._id !== shouldDeleteConfirmationApear)
+      );
+    }
+  };
 
   return (
     <div className="mt-20 py-20 wrapper">
@@ -37,7 +63,7 @@ const ManageMoviesPage = () => {
         </div>
       )}
 
-      {movies && (
+      {allMovies && (
         <table className="mt-5 min-h-screen w-full">
           <thead>
             <tr className="border-b border-gray-600 text-left">
@@ -49,7 +75,7 @@ const ManageMoviesPage = () => {
             </tr>
           </thead>
           <tbody>
-            {movies.map((movie, i) => (
+            {allMovies.map((movie, i) => (
               <tr key={movie._id} className="border-b border-gray-600">
                 <td>{i + 1}</td>
                 <td>{movie.title}</td>
@@ -57,7 +83,28 @@ const ManageMoviesPage = () => {
                 <td>{movie.year}</td>
                 <td>
                   <button className="btn mr-5">Update</button>
-                  <button className="btn">Delete</button>
+                  {shouldDeleteConfirmationApear === movie._id ? (
+                    <>
+                      <button
+                        className="btn mr-5"
+                        onClick={() => setShouldDeleteConfirmationApear(null)}
+                      >
+                        Cancel
+                      </button>
+                      <button className="btn" onClick={handleDelete}>
+                        Confirm
+                      </button>
+                    </>
+                  ) : (
+                    <button
+                      onClick={() =>
+                        setShouldDeleteConfirmationApear(movie._id)
+                      }
+                      className="btn"
+                    >
+                      Delete
+                    </button>
+                  )}
                 </td>
               </tr>
             ))}
